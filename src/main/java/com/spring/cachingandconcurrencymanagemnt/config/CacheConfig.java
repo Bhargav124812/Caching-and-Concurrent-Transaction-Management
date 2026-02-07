2eb4827074738243c36db62f40a7f8af908c2dc2
Bhargav <gudurusaibhargav2002@gmail.com>
2026-02-07 17:04:07 +0530

    Redis Cache in Spring Boot

--- src/main/java/com/spring/cachingandconcurrencymanagemnt/config/CacheConfig.java
+++ src/main/java/com/spring/cachingandconcurrencymanagemnt/config/CacheConfig.java
@@ -6,6 +6,13 @@ import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.cache.CacheManager;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import tools.jackson.databind.ObjectMapper;
 
 import java.time.Duration;
 
@@ -13,9 +20,18 @@ import java.time.Duration;
 @EnableCaching
 public class CacheConfig {
 
+
     @Bean
-    public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("employees", "users");
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .prefixCacheNameWith("my-redis")
+                .entryTtl(Duration.ofSeconds(60))
+                .enableTimeToIdle();
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
     }
 
 }
